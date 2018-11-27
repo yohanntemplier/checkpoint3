bdea28587826ea4cfcc628468dc0f90cdcdca185
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2018-11-27 22:49:14 +0100

    init projet

---
+++ src/Controller/MapController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Tile;
+use App\Repository\BoatRepository;
+
+class MapController extends AbstractController
+{
+    /**
+     * @Route("/map", name="map")
+     */
+    public function displayMapAction(BoatRepository $boatRepository)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $tiles = $em->getRepository(Tile::class)->findAll();
+
+        foreach ($tiles as $tile) {
+            $map[$tile->getCoordX()][$tile->getCoordY()] = $tile;
+        }
+
+        $boat = $boatRepository->findOneBy([]);
+
+        return $this->render('map/index.html.twig', [
+            'map'  => $map ?? [],
+            'boat' => $boat,
+        ]);
+    }
+}
---
+++ src/Migrations/Version20181123132219.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181123132219 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE boat (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, coord_x INT NOT NULL, coord_y INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tile (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, coord_x INT NOT NULL, coord_y INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE boat');
+        $this->addSql('DROP TABLE tile');
+    }
+}
